d015011d2cbea9e4ef63ed88e6ce9538d438f5e0
Brancucci <brancucci@mail.usf.edu>
2017-04-13 13:27:41 -0400

    updated database

--- mathgame/mathisfun/models.py
+++ mathgame/mathisfun/models.py
@@ -6,13 +6,10 @@ from django.db import models
 
 
 class Results(models.Model):
-    userid = models.CharField(max_length=10)
-    addition = models.FloatField(default=0)
-    subtraction = models.FloatField(default=0)
-    multiplication = models.FloatField(default=0)
-    division = models.FloatField(default=0)
-    total = models.FloatField(default=0)
+    userName = models.CharField(max_length=10)
+    operator = models.IntegerField(default=0)
+    average = models.FloatField(default=0.0)
 
     def __str__(self):
-        return self.userid
+        return self.userName
 
