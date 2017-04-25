be2d570eeefaa72146aaaaa399bec4b60d951a76
Brancucci <brancucci@mail.usf.edu>
2017-04-25 11:56:10 -0400

    added module doc string

---
+++ mathgame/mathisfun/__init__.py
@@ -0,0 +1,20 @@
+"""
+Project: Python Group Project
+
+Collaborators: Anthony Brancucci, Andrew Armstrong, John Donald, Natanael Rivera
+
+This module consists of a math game where users can login and compete with other users.
+Users can use a fraction calculator, take fraction quiz for four operations, and compare
+results with other users.
+
+Classes:
+    ChartData(APIView): Generates a chart for displaying user results
+
+Functions:
+    login_view(request):    Allows users to login
+    selection(request):     Allows users to select available game options
+    solver(request):        A fraction calculator
+    quizzer(request):       Quizzes users on fractions
+    results(request):       Displays results of user scores
+
+"""
